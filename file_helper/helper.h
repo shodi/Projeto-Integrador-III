56905ad2cb8132572d3838c5dd74bda3a7918fdf
Henrique Shodi Maeta <japa1996@hotmail.com>
2017-03-03 00:33:14 -0300

    Leitura de arquivos funcionando

--- file_helper/helper.h
+++ file_helper/helper.h
@@ -12,8 +12,8 @@ typedef struct config{
 
 }Config;
 
-void read_file(char *file_name, int id){
-    Config* options;
+Config* read_file(char *file_name, int id){
+    Config* options = (Config *) malloc (1 * sizeof(Config));
     FILE *file;
     char str_id[ARR_SIZE];
     sprintf(str_id, "%d", id);
@@ -36,17 +36,26 @@ void read_file(char *file_name, int id){
             case 0:
                 options->_id = atoi(line);
                 break;
+            case 1:
+                options->__arrival = atoi(line);
+                break;
+            case 2:
+                options->__steps = line;
+                break;
         }
     }
 
     fclose(file);
 
-    // return options;
+    return options;
 }
 
-int main(int argc, char *argv[]){
+// int main(int argc, char *argv[]){
     
-    read_file(argv[1], 1);
-
-    return 0;
-}
+//     Config* options;
+//     options = read_file(argv[1], 1);
+//     printf("id: %d", options->_id);
+//     printf("arrival: %d", options->__arrival);
+//     printf("Sequence: %s", options->__steps);
+//     return 0;
+// }
---
+++ scripts/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+def main(file_name):
+	with open(file_name, 'r') as fp:
+		line = fp.readline()
+		while line != '':
+			
+							
+
+if __name__ == '__main__':
+	main(sys.argv[1])
